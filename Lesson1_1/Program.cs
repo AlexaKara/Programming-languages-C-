e28a0dc28aacc26950687357935b99ffb5c0cab6
Alexa <alexa411@mail.ru>
2022-11-21 22:14:16 +0300

    readme

--- Lesson1_1/Program.cs
+++ Lesson1_1/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M master
+git remote add origin https://github.com/AlexaKara/Programming-languages-C-.git
+git push -u origin master
