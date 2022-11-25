f3934ae0f9eb2db8e7176a8def429ebbb4882901
Alexa <alexa411@mail.ru>
2022-11-25 16:39:31 +0300

    add code

---
+++ Lesson2_3/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введи цифру, обозначающую день недели: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+
+if(1 <= dayNumber & dayNumber <= 5)
+{
+  Console.WriteLine("Будний день");
+}
+if(6 <= dayNumber & dayNumber <= 7)
+{
+  Console.WriteLine("Выходной день");
+}
+if(dayNumber > 7)
+{
+  Console.WriteLine("Введи корректные данные");
+}
