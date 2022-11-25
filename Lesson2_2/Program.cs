5831219509951338b5e982e663410c4e9e3e15dc
Alexa <alexa411@mail.ru>
2022-11-25 15:21:13 +0300

    agg code

---
+++ Lesson2_2/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введи число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+string room = Convert.ToString(number);
+if (room.Length > 2){
+  Console.WriteLine("третья цифра -> " + room[2]);
+}
+else {
+  Console.WriteLine("-> третьей цифры нет");
+}
