3f6e3d17d2f84889d808911c776e543d1ac82524
Alexa <alexa411@mail.ru>
2022-11-28 00:25:25 +0300

    add lesson4_3

---
+++ Lesson4_2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int n = number;
+int sum = 0;
+while(n > 0)
+{
+    sum = sum + n % 10;
+    n = n / 10;        
+}
+Console.WriteLine(sum);
