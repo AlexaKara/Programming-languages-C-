89e8f806570a6eda9cba07007ccf01740f557364
Alexa <alexa411@mail.ru>
2022-11-28 00:48:16 +0300

    add lesson4_3

---
+++ Lesson4_3/Program.cs
@@ -0,0 +1,33 @@
+﻿int[] CreateArray(int array)
+{
+    return new int[array];
+}
+
+void Fill(int[] array, int min, int max)
+{ 
+    int size = array.Length; 
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(min, max);
+    }
+}
+
+string PrintGood(int[] numbers)
+    {
+        int size = numbers.Length;
+        int i = 0;
+        string result = "[ ";
+
+        while (i < size)
+        {
+            result += ($"{numbers[i],3} ");
+            i++;
+        }
+        return result + "]";
+    }
+
+
+int[] array = CreateArray(8);
+Console.WriteLine(PrintGood(array));
+Fill(array, 1, 20); 
+Console.WriteLine(PrintGood(array)); 
