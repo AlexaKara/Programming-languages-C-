cf74bf5f2ea740893b2d6d1539797ef592214c1d
Alexa <alexa411@mail.ru>
2022-11-30 02:03:00 +0300

    change Lesson5_2

--- Lesson5_2/Program.cs
+++ Lesson5_2/Program.cs
@@ -1,43 +1,13 @@
-﻿int[] Array(int count)
-    {
-     return new int[count];
-    }
-
-void Fill(int[] array, int min, int max)
-{ 
-    int size = array.Length; 
-    for (int i = 0; i < size; i++)
-    {
-        array[i] = new Random().Next(min, max + 1);
-    }
-}
-
-string PrintGood(int[] numbers)
-{
-    int size = numbers.Length;
-    int i = 0;
-    string result = "[ ";
-
-    while (i < size)
-    {
-        result += ($"{numbers[i],3} ");
-        i++;
-    }
-    return result + "]";
-}
-
-int SumNegativ(int[] array)
-{
-    int result = 0;
-    int size = array.Length;
-    for (int i = 1; i <= size; i = i + 2)
-    {
-        result = result + array[i];
-    }
-    return result;
-}
-
-int[] array = Array(10);
-Fill(array, -5, 5);
-int result = SumNegativ(array);
-Console.WriteLine($"{PrintGood(array)} -> {result}");
+﻿        var rnd = new Random();
+            var arr = new int[11];
+ 
+            for (int i = 0; i < arr.Length; i++)
+                Console.Write("{0}\t", arr[i] = rnd.Next(-10, 10));
+            Console.WriteLine(new Random());
+ 
+        
+            var odd = arr.Where((element, index) => index%2!=0);
+            
+            Console.WriteLine("Элементов нечетных позиций: Количество: {0}. Сумма: {1}.", odd.Count(), odd.Sum());
+ 
+            Console.ReadKey();
--- Library.cs
+++ Library.cs
@@ -84,3 +84,20 @@ int SumNegative(int[] numbers)
   }
   return result;
 }
+
+
+
+//сумма элементов, стоящих на нечётных позициях в рандомном массиве.
+        var rnd = new Random();
+            var arr = new int[11];
+ 
+            for (int i = 0; i < arr.Length; i++)
+                Console.Write("{0}\t", arr[i] = rnd.Next(-10, 10));
+            Console.WriteLine(new Random());
+ 
+        
+            var odd = arr.Where((element, index) => index%2!=0);
+            
+            Console.WriteLine("Элементов нечетных позиций: Количество: {0}. Сумма: {1}.", odd.Count(), odd.Sum());
+ 
+            Console.ReadKey();
