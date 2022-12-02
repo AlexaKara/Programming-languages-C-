c9d85432b9a7fd4226dd6270c51267b8873cdfe6
Alexa <alexa411@mail.ru>
2022-12-02 16:47:09 +0300

    add code

---
+++ Lesson7_2/Program.cs
@@ -0,0 +1,35 @@
+﻿int[,] CreateTriangle(int row)
+{
+  int[,] triangle = new int[row, row];
+  for (int i = 0; i < row; i++)
+  {
+    triangle[i, 0] = 1;
+    triangle[i, i] = 1;
+  }
+
+  for (int i = 2; i < row; i++)
+  {
+    for (int j = 1; j <= i; j++)
+    {
+      triangle[i, j] =
+      triangle[i - 1, j - 1] + triangle[i - 1, j];
+    }
+  }
+  return triangle;
+}
+
+void PrintTriangle(int[,] triangle)
+{
+    for (int i = 0; i < triangle.GetLength(0); i++)
+  {
+    for (int j = 0; j < triangle.GetLength(1); j++)
+    {
+      if (triangle[i, j] % 2 == 0) Console.Write(" ");
+      else Console.Write("*");
+    }
+    Console.WriteLine();
+  }
+}
+
+int[,] tr = CreateTriangle(120);           
+PrintTriangle(tr);
