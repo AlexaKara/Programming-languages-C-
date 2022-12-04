e10ca9dc9d169fc4f30848f9a3671fb55cf2e6a2
Alexa <alexa411@mail.ru>
2022-12-05 00:19:22 +0300

    add code

---
+++ Lesson9_3/Program.cs
@@ -0,0 +1,12 @@
+﻿int AckermanFunction (int m, int n)
+{
+if (m == 0) return n + 1;
+if (m != 0 && n == 0) return AckermanFunction(m - 1, 1);
+if (m > 0 && n > 0) return AckermanFunction(m - 1, AckermanFunction(m, n - 1));
+return AckermanFunction(m, n);
+}
+
+
+int m = 3;
+int n = 2;
+Console.WriteLine($"Функция Аккермана для чисел m,n: A({m},{n}) = {AckermanFunction(m, n)}");
