c5ac93d569c3dc77a6397d216a7fd36e9561e901
Alexa <alexa411@mail.ru>
2022-12-05 00:11:15 +0300

    add code

---
+++ Lesson9_1/Program.cs
@@ -0,0 +1,12 @@
+﻿void Number (int N)
+{
+    if(N < 0) Console.Write($"{N} не натуральное число");
+    else 
+    {
+        int i = 1;
+        Console.Write($"{N} ");
+        if(i != N) Number(N - 1);
+    }
+}
+int N = 8;
+Number(N);
