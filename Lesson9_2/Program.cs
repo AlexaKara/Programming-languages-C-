46304e5ba1c3b519253bfcbdb6b6e6c3556946e2
Alexa <alexa411@mail.ru>
2022-12-05 00:14:46 +0300

    add code

---
+++ Lesson9_2/Program.cs
@@ -0,0 +1,11 @@
+﻿int SumNumbers(int M, int N)
+{
+    if (M == N) return M;                      
+        else if (M < N) return N + SumNumbers(M, N - 1); 
+        else if (N == 0 && M == 0) return (M * (M + 1)) / 2;    
+        else return N + SumNumbers(M, N + 1);      
+}
+
+int M = 1;
+int N = 15;
+Console.WriteLine($"Сумма натуральных элементов в промежутке от M({M}) до N({N}) = {SumNumbers(M, N)}");;
